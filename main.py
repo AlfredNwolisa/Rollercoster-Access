5419b34015c2f58cf0534b516ee6b251f3b5b863
alfieN1 <>
2023-04-11 17:06:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height == 120:
+  print("you can ride the roller coaster")
+elif height  <= 150:
+  print("upcoming tallie")
+elif height <= 200:
+  print("oga calm down na")  
+elif height > 200:
+  print("bros de calm down na, abeg o")
+else:
+  print("sorry biatch get tall loser\
+  yes i said so")
